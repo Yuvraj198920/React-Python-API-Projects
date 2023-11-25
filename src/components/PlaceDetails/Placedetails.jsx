6968b23021bf6218c622d0e9ab2e8d010f586392
Yuvraj198920 <yuvvraaj0flying@gmail.com>
2023-11-25 16:16:49 +0530

    Display cuisine and rating

--- src/components/PlaceDetails/Placedetails.jsx
+++ src/components/PlaceDetails/Placedetails.jsx
@@ -22,12 +22,40 @@ const PlaceDetails = ({ place }) => {
         height="194"
         title={place.name}
         style={{ height: 350 }}
-        image={place.photo ? place.photo.images.large.url : ""}
+        image={
+          place.photo
+            ? place.photo.images.large.url
+            : "https://fastly.picsum.photos/id/23/3887/4899.jpg?hmac=2fo1Y0AgEkeL2juaEBqKPbnEKm_5Mp0M2nuaVERE6eE"
+        }
       ></CardMedia>
       <CardContent>
-        <Typography gutterBottom variant="body2" color="text.secondary">
+        <Typography gutterBottom variant="subtitle1">
           {place.name}
         </Typography>
+        <Box display="flex" justifyContent="space-between">
+          <Typography variant="body2" color="text.secondary">
+            Price
+          </Typography>
+          <Typography gutterBottom variant="body2" color="text.secondary">
+            {place.price_level}
+          </Typography>
+        </Box>
+        <Box display="flex" justifyContent="space-between">
+          <Typography variant="body2" color="text.secondary">
+            Ranking
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {place.ranking}
+          </Typography>
+        </Box>
+        <Box display="flex" justifyContent="space-between" my={1}>
+          <Typography variant="body2" color="text.secondary">
+            Cuisine
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {place?.cuisine?.map((c) => c.name).join(",")}
+          </Typography>
+        </Box>
       </CardContent>
     </Card>
   );
