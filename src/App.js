c4b52d36b016c9ebbef8fbba83b5c5f8af671338
Yuvraj198920 <yuvvraaj0flying@gmail.com>
2023-11-25 08:23:57 +0530

    starting for scratch

--- src/App.js
+++ src/App.js
@@ -1,58 +1,7 @@
-import React, { useEffect, useState } from "react";
-
-import Header from "./components/Header/Header";
-import Map from "./components/Map/Map";
-import List from "./components/List/List";
-
-import { CssBaseline, Grid } from "@mui/material";
-import { getPlacesData } from "./api";
+import React from "react";
 
 const App = () => {
-  const [placedata, setPlacedata] = useState([]);
-  const [coordinates, setCoordinates] = useState({});
-  const [bounds, setBounds] = useState({});
-  const [childClicked, setChildClicked] = useState(null);
-
-  function handleChange(e) {
-    setChildClicked(e);
-  }
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      ({ coords: { latitude, longitude } }) => {
-        setCoordinates({ lat: latitude, lng: longitude });
-      }
-    );
-  }, []);
-
-  useEffect(() => {
-    console.log(coordinates, bounds);
-    getPlacesData(bounds.sw, bounds.ne).then((data) => {
-      setPlacedata(data);
-      console.log(placedata);
-    });
-  }, [coordinates, bounds]);
-
-  return (
-    <>
-      <CssBaseline />
-      <Header />
-      <Grid container spacing={3} style={{ width: "100%" }}>
-        <Grid item xs={12} md={4}>
-          <List places={placedata} childClickedtoList={childClicked} />
-        </Grid>
-
-        <Grid item xs={12} md={8}>
-          <Map
-            setChildClickedMap={setChildClicked}
-            setCoordinates={setCoordinates}
-            setBounds={setBounds}
-            coordinates={coordinates}
-            places={placedata}
-          />
-        </Grid>
-      </Grid>
-    </>
-  );
+  return <h1>Travel advisor</h1>;
 };
 
 export default App;
